092596642198ecff87747a5f9d391db21e266153
jbilander <jorgen.bilander@creang.com>
2018-03-30 17:53:04 +0200

    rename

--- src/main/java/com/creang/service/db/UpdateRaceService.java
+++ src/main/java/com/creang/service/db/UpdateRaceService.java
@@ -29,55 +29,55 @@ public class UpdateRaceService {
 
             conn.setAutoCommit(false);
 
-            try (PreparedStatement stmt1 = conn.prepareStatement(sql1)) {
-                try (PreparedStatement stmt2 = conn.prepareStatement(sql2)) {
-                    try (PreparedStatement stmt3 = conn.prepareStatement(sql3)) {
-                        try (PreparedStatement stmt4 = conn.prepareStatement(sql4)) {
+            try (PreparedStatement ps1 = conn.prepareStatement(sql1)) {
+                try (PreparedStatement ps2 = conn.prepareStatement(sql2)) {
+                    try (PreparedStatement ps3 = conn.prepareStatement(sql3)) {
+                        try (PreparedStatement ps4 = conn.prepareStatement(sql4)) {
 
                             for (Race race : races) {
 
-                                stmt1.setString(1, Util.getLocalDateTimeString(race.getUpdated()));
-                                stmt1.setBoolean(2, race.isHasResult());
-                                stmt1.setString(3, race.getTrackState());
-                                stmt1.setString(4, race.getTrackSurface());
-                                stmt1.setBigDecimal(5, race.getWinTurnOver());
-                                stmt1.setInt(6, race.getId());
-                                stmt1.addBatch();
+                                ps1.setString(1, Util.getLocalDateTimeString(race.getUpdated()));
+                                ps1.setBoolean(2, race.isHasResult());
+                                ps1.setString(3, race.getTrackState());
+                                ps1.setString(4, race.getTrackSurface());
+                                ps1.setBigDecimal(5, race.getWinTurnOver());
+                                ps1.setInt(6, race.getId());
+                                ps1.addBatch();
 
                                 for (Participant participant : race.getParticipants()) {
 
-                                    stmt2.setBoolean(1, participant.isScratched());
-                                    stmt2.setBoolean(2, participant.isDriverChanged());
-                                    stmt2.setBigDecimal(3, participant.getWinnerOdds());
-                                    stmt2.setBigDecimal(4, participant.getInvestmentWinner());
-                                    stmt2.setBigDecimal(5, participant.getCardWeight());
-                                    stmt2.setBoolean(6, participant.isCardWeightChanged());
-                                    stmt2.setInt(7, participant.getId());
-                                    stmt2.addBatch();
+                                    ps2.setBoolean(1, participant.isScratched());
+                                    ps2.setBoolean(2, participant.isDriverChanged());
+                                    ps2.setBigDecimal(3, participant.getWinnerOdds());
+                                    ps2.setBigDecimal(4, participant.getInvestmentWinner());
+                                    ps2.setBigDecimal(5, participant.getCardWeight());
+                                    ps2.setBoolean(6, participant.isCardWeightChanged());
+                                    ps2.setInt(7, participant.getId());
+                                    ps2.addBatch();
 
-                                    stmt3.setInt(1, participant.getHorse().getForeShoes());
-                                    stmt3.setInt(2, participant.getHorse().getHindShoes());
-                                    stmt3.setInt(3, participant.getHorse().getId());
-                                    stmt3.addBatch();
+                                    ps3.setInt(1, participant.getHorse().getForeShoes());
+                                    ps3.setInt(2, participant.getHorse().getHindShoes());
+                                    ps3.setInt(3, participant.getHorse().getId());
+                                    ps3.addBatch();
 
                                     if (participant.isDriverChanged()) {
 
-                                        stmt4.setInt(1, participant.getDriver().getKeyId());
-                                        stmt4.setString(2, participant.getDriver().getFirstName());
-                                        stmt4.setString(3, participant.getDriver().getLastName());
-                                        stmt4.setString(4, participant.getDriver().getShortName());
-                                        stmt4.setBoolean(5, participant.getDriver().isAmateur());
-                                        stmt4.setBoolean(6, participant.getDriver().isApprenticeAmateur());
-                                        stmt4.setBoolean(7, participant.getDriver().isApprenticePro());
-                                        stmt4.setInt(8, participant.getDriver().getId());
-                                        stmt4.addBatch();
+                                        ps4.setInt(1, participant.getDriver().getKeyId());
+                                        ps4.setString(2, participant.getDriver().getFirstName());
+                                        ps4.setString(3, participant.getDriver().getLastName());
+                                        ps4.setString(4, participant.getDriver().getShortName());
+                                        ps4.setBoolean(5, participant.getDriver().isAmateur());
+                                        ps4.setBoolean(6, participant.getDriver().isApprenticeAmateur());
+                                        ps4.setBoolean(7, participant.getDriver().isApprenticePro());
+                                        ps4.setInt(8, participant.getDriver().getId());
+                                        ps4.addBatch();
                                     }
                                 }
                             }
-                            stmt4.executeBatch();
-                            stmt3.executeBatch();
-                            stmt2.executeBatch();
-                            stmt1.executeBatch();
+                            ps4.executeBatch();
+                            ps3.executeBatch();
+                            ps2.executeBatch();
+                            ps1.executeBatch();
                         }
                     }
                 }
